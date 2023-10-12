daf218bebaf901e023110f94786729a876e029b7
João Lucas MAIN <joaolucasmmd@gmail.com>
2023-10-12 12:45:49 -0300

    cadastro adicionado

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function login() {
 }
 
 
+function register() {
+    var email = document.getElementById("userEmail").value;
+    var password = document.getElementById("userPassword").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(userCredential) {
+            var user = userCredential.user;
+            alert("Cadastro bem-sucedido!");
+            console.log(user);
+        })
+        .catch(function(error) {
+            alert("Erro no cadastro: " + error.message);
+        });
+}
